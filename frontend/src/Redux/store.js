2af0824109c7e7ca077b3306d46db261970a2bf1
unknown <jkaushik0802@gmail.com>
2021-02-21 03:57:40 +0530

    created Register Page & Redux

---
+++ frontend/src/Redux/store.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { registerReducer } from "../Components/JobSeeker/Register/RegisterRedux/reducer";
+const rootReducer = combineReducers({
+  register: registerReducer,
+});
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
