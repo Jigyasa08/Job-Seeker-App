dcf4e750dfbfa6c9887f82f074c9abb9a3059d36
unknown <jkaushik0802@gmail.com>
2021-02-22 15:25:14 +0530

    UserAPI

---
+++ backend/UserAPI/data/data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: 1,
+    name: "Jigyasa",
+    title: "Full Stack Developer",
+    jobDescription:
+      "An empathetic & hardworking individual with a problem-solving mindset who likes building apps.",
+    email: "jigyasa@mern.com",
+    phone: "1234567890",
+    experience: "Fresher",
+    location: "Gurgaon",
+    skills: "HTML, CSS, JavaScript, React, Redux",
+    cv: [],
+    img: [],
+  },
+  {
+    id: 2,
+    name: "Soumya",
+    title: "Research Analyst",
+    jobDescription:
+      "An empathetic & hardworking individual with a problem-solving mindset who likes building apps.",
+    email: "soumya@mern.com",
+    phone: "1234567890",
+    experience: "3+",
+    location: "Gurgaon",
+    skills: "None",
+    cv: [],
+    img: [],
+  },
+];
+
+module.exports = data;
